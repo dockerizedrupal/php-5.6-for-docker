ed3913a7e2739ee139d1e700f2640cadf1339d02
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-18 13:30:33 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -88,6 +88,11 @@ class php {
     require => File['/phpfarm/src/custom-options-5.5.15.sh']
   }
 
+  exec { 'rm -rf /phpfarm/src/php-5.5.15':
+    path => ['/bin'],
+    require => Exec['/phpfarm/src/compile.sh 5.5.15']
+  }
+
   file { '/phpfarm/inst/php-5.5.15/etc/php-fpm.conf':
     ensure => present,
     source => '/tmp/build/phpfarm/inst/php-5.5.15/etc/php-fpm.conf',
