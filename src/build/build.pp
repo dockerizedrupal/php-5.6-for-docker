705360d84ae1d387d23db88488fb5febbd0a10ed
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-16 21:13:34 +0200

    Update

--- src/build/build.pp
+++ src/build/build.pp
@@ -1,9 +1,5 @@
 node default {
   include php
-  include php::extension::xdebug
-  include php::extension::memcached
-  include php::extension::redis
-  include php::extension::apcu
+  include php::extensions
   include php::drush
-  include php::extension::blackfire
 }
