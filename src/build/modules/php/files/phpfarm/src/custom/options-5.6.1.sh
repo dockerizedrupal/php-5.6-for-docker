12b9f2754170dcafb069ec14273c18d24f9be24e
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-03 11:12:52 +0200

    Update

--- src/build/modules/php/files/phpfarm/src/custom/options-5.6.1.sh
+++ src/build/modules/php/files/phpfarm/src/custom/options-5.6.1.sh
@@ -30,4 +30,5 @@ configoptions="--with-libdir=/lib/x86_64-linux-gnu \
   --with-gettext \
   --with-pspell \
   --with-ldap=/usr \
+  --with-config-file-scan-dir=/phpfarm/inst/php-5.6.1/etc/conf.d \
 "
