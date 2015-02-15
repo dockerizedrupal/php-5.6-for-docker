e8ca34326005e59b87a3f88365b41dfba4cb9325
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-15 12:36:17 +0200

    Update

--- src/build/modules/php/manifests/extension/redis.pp
+++ src/build/modules/php/manifests/extension/redis.pp
@@ -7,26 +7,26 @@ class php::extension::redis {
     source => 'puppet:///modules/php/tmp/redis-2.2.5.tgz'
   }
 
-  exec { 'cd /tmp && tar xzf redis-2.2.5.tgz':
+  bash_exec { 'cd /tmp && tar xzf redis-2.2.5.tgz':
     require => File['/tmp/redis-2.2.5.tgz']
   }
 
-  exec { 'cd /tmp/redis-2.2.5 && phpize-5.6.1':
-    require => Exec['cd /tmp && tar xzf redis-2.2.5.tgz']
+  bash_exec { 'cd /tmp/redis-2.2.5 && phpize-5.6.1':
+    require => Bash_exec['cd /tmp && tar xzf redis-2.2.5.tgz']
   }
 
-  exec { 'cd /tmp/redis-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.6.1 --enable-redis-igbinary':
+  bash_exec { 'cd /tmp/redis-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.6.1 --enable-redis-igbinary':
     timeout => 0,
-    require => Exec['cd /tmp/redis-2.2.5 && phpize-5.6.1']
+    require => Bash_exec['cd /tmp/redis-2.2.5 && phpize-5.6.1']
   }
 
-  exec { 'cd /tmp/redis-2.2.5 && make':
+  bash_exec { 'cd /tmp/redis-2.2.5 && make':
     timeout => 0,
-    require => Exec['cd /tmp/redis-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.6.1 --enable-redis-igbinary']
+    require => Bash_exec['cd /tmp/redis-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.6.1 --enable-redis-igbinary']
   }
 
-  exec { 'cd /tmp/redis-2.2.5 && make install':
+  bash_exec { 'cd /tmp/redis-2.2.5 && make install':
     timeout => 0,
-    require => Exec['cd /tmp/redis-2.2.5 && make']
+    require => Bash_exec['cd /tmp/redis-2.2.5 && make']
   }
 }
