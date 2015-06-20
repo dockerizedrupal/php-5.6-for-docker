d00d7476fac3a3b1749ddbf07c238b3b4ab4ca8a
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-20 09:25:52 +0300

    Update

--- src/php/build/modules/php/manifests/init.pp
+++ src/php/build/modules/php/manifests/init.pp
@@ -23,7 +23,7 @@ class php {
 
   file { '/usr/local/src/phpfarm/src/custom/options-5.6.10.sh':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/src/custom/options-5.6.10.sh',
+    source => 'puppet:///modules/php/usr/local/src/phpfarm/src/custom/options-5.6.10.sh',
     mode => 755,
     require => Bash_exec['mv /tmp/php-5.6.10 /usr/local/src/phpfarm/src/php-5.6.10']
   }
@@ -39,7 +39,7 @@ class php {
 
   file { '/usr/local/src/phpfarm/inst/php-5.6.10/etc/php-fpm.conf':
     ensure => present,
-    source => 'puppet:///modules/php/phpfarm/inst/php-5.6.10/etc/php-fpm.conf',
+    source => 'puppet:///modules/php/usr/local/src/phpfarm/inst/php-5.6.10/etc/php-fpm.conf',
     mode => 644,
     require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 5.6.10']
   }
