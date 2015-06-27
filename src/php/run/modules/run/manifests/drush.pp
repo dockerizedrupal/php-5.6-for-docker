ac871084d9c327afea52eab5c89116cf118ea5d2
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-27 20:23:34 +0300

    Update

--- src/php/run/modules/run/manifests/drush.pp
+++ src/php/run/modules/run/manifests/drush.pp
@@ -1,4 +1,6 @@
 class run::drush {
+  require run::user
+
   if ($drupal_version == '7') or ($drupal_version == '8') {
     file { '/usr/local/bin/drush':
       ensure => link,
@@ -9,5 +11,8 @@ class run::drush {
       ensure => link,
       target => '/usr/local/src/drush7/drush.complete.sh'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/drush7": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/drush7": }
   }
 }
