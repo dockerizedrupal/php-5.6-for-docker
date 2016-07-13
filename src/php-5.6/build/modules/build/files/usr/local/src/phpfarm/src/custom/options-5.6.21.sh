a2d3f6684d9e879524cafbd027ca4c07b1c20e74
Jürgen Viljaste <j.viljaste@gmail.com>
2016-07-13 14:35:35 +0300

    #42

--- src/php-5.6/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.6.21.sh
+++ src/php-5.6/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.6.21.sh
@@ -36,4 +36,5 @@ configoptions="--with-libdir=/lib/x86_64-linux-gnu \
   --with-config-file-scan-dir=/usr/local/src/phpfarm/inst/php-5.6.21/etc/conf.d \
   --with-mssql=/usr/local/freetds \
   --with-readline \
+  --with-freetype-dir \
 "
